dcca9d25c237fa34e76535dc2a3618a6c1c046bd
鞠淼 <miao1982528@163.com>
2018-03-12 17:20:27 +0800

    add new files

---
+++ src/HelloWorld/HelloWorld.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HelloWorld;
+
+/**
+ * This is just an example.
+ */
+class SayHello{
+    public static function world() {
+        return 'Hello World, Composer from Time_Lover!';
+    }
+}
